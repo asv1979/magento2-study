7d6ceae3d0344bda9e93861d23ec6250698dee3a
Alexey Vasilyev home <asv2108@gmail.com>
2019-12-15 14:37:56 +0200

    change files permitions

--- app/etc/config.php
+++ app/etc/config.php
@@ -218,7 +218,7 @@ return [
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
-        'Asv_Crud' => 1,
+        'Asv_Test' => 1,
         'Dotdigitalgroup_Email' => 1,
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
